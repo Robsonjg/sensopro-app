c02b792ee7fa46260a496b84eb1c259bd88e4f50
Robson Gonçalves <robsonjg6@gmail.com>
2026-05-29 14:35:46 -0300

    fix css not loading in production

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,7 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+   base: "/",
   plugins: [react()],
 
   resolve: {
@@ -18,7 +19,8 @@ export default defineConfig({
 },
 
   build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
+  outDir: "dist",
+  emptyOutDir: true,
+  assetsDir: "assets",
+},
 });

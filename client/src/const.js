8a105d88383e4e29cf4679eb87ac2dea68319221
Robson Gonçalves <robsonjg6@gmail.com>
2026-06-19 10:59:02 -0300

    fix: atualiza definicao do banco e desativa prepared statements 19-06-2026 10:58

--- client/src/const.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = import.meta.env.VITE_API_URL ||
-    "http://localhost:3001/api/trpc";
-export const COOKIE_NAME = "app_session_id";
-export const ONE_YEAR_MS = 1000 * 60 * 60 * 24 * 365;
-export const getLoginUrl = () => "/admin/login";

46b5ad23286c488c48b786364407cda693810f31
Robson José Gonçalves <104995555+Robsonjg@users.noreply.github.com>
2026-06-19 00:46:18 -0300

    Update trpc.ts

--- client/src/lib/trpc.ts
+++ client/src/lib/trpc.ts
@@ -1,6 +1,5 @@
 import { createTRPCReact } from "@trpc/react-query";
 import type { AppRouter } from "../../../server/routers";
-
 import { httpBatchLink } from "@trpc/client";
 
 export const trpc = createTRPCReact<AppRouter>();
@@ -8,7 +7,8 @@ export const trpc = createTRPCReact<AppRouter>();
 export const trpcClient = trpc.createClient({
   links: [
     httpBatchLink({
-      url: `${import.meta.env.VITE_API_URL ?? ""}/api/trpc`,
+      // Removido o "/api/trpc" daqui, já que ele já vem de dentro da variável
+      url: `${import.meta.env.VITE_API_URL ?? ""}`,
     }),
   ],
 });
